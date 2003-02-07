01409fbcea2de888a7b7370dfc9bd59a8fb8e867
geoff <>
2003-02-07 15:59:42 +0000

    Made APC work correctly

--- apc.py
+++ apc.py
@@ -4,13 +4,21 @@ def getConfig(optionMap):
     return ApcConfig(optionMap)
 
 class ApcConfig(optimization.OptimizationConfig):
-
     def getLibraryFile(self):
         return os.path.join("data", "apc", carmen.architecture, "libapc.a")
-
     def getSubPlanFileName(self, test, sourceName):
-        return os.path.join(test.options, "APC_FILES", sourceName)
+        return os.path.join(test.options.split()[0], sourceName)
     def getTestCollator(self):
-        return optimization.OptimizationConfig.getTestCollator(self) + [ optimization.ExtractSubPlanFile(self, "status", "status") ]
+        return optimization.OptimizationConfig.getTestCollator(self) + [ RemoveLogs(), optimization.ExtractSubPlanFile(self, "status", "status") ]
 
+class RemoveLogs:
+    def __repr__(self):
+        return "Removing log files for"
+    def __call__(self, test, description):
+        self.removeFile(test, "errors")
+        self.removeFile(test, "output")
+    def removeFile(self, test, stem):
+        os.remove(test.getTmpFileName(stem, "r"))
+    def setUpSuite(self, suite, description):
+        pass
 

1cb7f80c0b0af5e532b524edd4e419e33b5a92b1
johana <>
2003-02-14 14:53:28 +0000

    special apc ruleset linking on linux for quick turnaround time

--- apc.py
+++ apc.py
@@ -1,4 +1,4 @@
-import carmen, os, sys, string, shutil, optimization
+import carmen, os, sys, stat, string, shutil, optimization
 
 def getConfig(optionMap):
     return ApcConfig(optionMap)
@@ -10,10 +10,12 @@ class ApcConfig(optimization.OptimizationConfig):
         return os.path.join(test.options.split()[0], sourceName)
     def getActionSequence(self):
         if self.optionMap.has_key("rulecomp"):
-            return [ ApcCompileRules(self.getRuleSetName) ]
+            return [ ApcCompileRules(self.getRuleSetName, self.getLibraryFile(), None, 1) ]
         
         staticFilter = carmen.UpdatedLocalRulesetFilter(self.getRuleSetName, self.getLibraryFile())
-        return [ ApcCompileRules(self.getRuleSetName, staticFilter) ] + carmen.CarmenConfig.getActionSequence(self)
+        return [ self.getCompileRules(staticFilter) ] + carmen.CarmenConfig.getActionSequence(self)
+    def getCompileRules(self, staticFilter):
+        return ApcCompileRules(self.getRuleSetName, self.getLibraryFile(), staticFilter, self.optionMap.has_key("rulecomp"))
     def getTestCollator(self):
         return optimization.OptimizationConfig.getTestCollator(self) + [ RemoveLogs(), optimization.ExtractSubPlanFile(self, "status", "status") ]
     def getRuleSetName(self, test):
@@ -27,20 +29,72 @@ class ApcConfig(optimization.OptimizationConfig):
         return None
 
 class ApcCompileRules(carmen.CompileRules):
-    def __init__(self, getRuleSetName, filter = None):
-        carmen.CompileRules.__init__(self, getRuleSetName, filter)
+    def __init__(self, getRuleSetName, libraryFile, filter = None, forcedRuleCompile = 0):
+        carmen.CompileRules.__init__(self, getRuleSetName, "-optimize", filter)
+        self.forcedRuleCompile = forcedRuleCompile
+        self.apcLib = os.path.join(os.environ["CARMSYS"], libraryFile)
     def __call__(self, test, description):
         carmTmpDir = os.environ["CARMTMP"]
         if not os.path.isdir(carmTmpDir):
             os.mkdir(carmTmpDir)
+        if self.forcedRuleCompile == 0 and carmen.architecture == "i386_linux":
+            self.linuxRuleSetBuild(test, description)
+        else:
+            carmen.CompileRules.__call__(self, test, description)
+
+    def linuxRuleSetBuild(self, test, description):
         ruleset = carmen.RuleSet(self.getRuleSetName(test), self.raveName)
-        if ruleset.isValid() and not ruleset.name in self.rulesCompiled:
-            print description + " -  ruleset " + ruleset.name
-            ruleset.backup()
+        if not ruleset.isValid() or ruleset.name in self.rulesCompiled:
+            return
+        apcExecutable = ruleset.targetFile
+        ruleLib = self.getRuleLib(ruleset.name)
+        if self.isNewer(apcExecutable, self.apcLib):
+            return
+        print description + " -  ruleset " + ruleset.name
+        ruleset.backup()
+        if not os.path.isfile(ruleLib):
             compiler = os.path.join(os.environ["CARMSYS"], "bin", "crc_compile")
-            commandLine = compiler + " " + self.raveName + " -optimize -archs " + carmen.architecture + " " + ruleset.sourceFile
-            self.rulesCompiled.append(ruleset.name)
-            os.system(commandLine)
+            os.system(self.ruleCompileCommand(ruleset.sourceFile))
+        commandLine = "g++ -pthread " + self.linkLibs(self.apcLib, ruleLib) + "-o " + apcExecutable
+        si, so, se = os.popen3(commandLine)
+        lastErrors = se.readlines()
+        if len(lastErrors) > 0:
+            if lastErrors[-1].find("exit status") != -1:
+                print "Building", ruleset.name, "failed!"
+                for line in lastErrors:
+                    print "   ", line.strip()
+                return
+        self.rulesCompiled.append(ruleset.name)
+
+    def getRuleLib(self, ruleSetName):
+        optArch = carmen.architecture + "_opt"
+        ruleLib = ruleSetName + ".a"
+        return os.path.join(os.environ["CARMTMP"], "compile", self.raveName.upper(), optArch, ruleLib)
+        
+    def ruleCompileCommand(self, sourceFile):
+       compiler = os.path.join(os.environ["CARMSYS"], "bin", "crc_compile")
+       params = " -optimize -makelib -archs " + carmen.architecture
+       return compiler + " " + self.raveName + params + " " + sourceFile
+                    
+    def linkLibs(self, apcLib, ruleLib):
+       path1 = os.path.join(os.environ["CARMSYS"], "data", "crc", carmen.architecture)
+       path2 = os.path.join(os.environ["CARMSYS"], "lib", carmen.architecture)
+       paths = " -L" + path1 + " -L" + path2
+       basicLibs = " -lxprs -lxprl -lm -ldl -lrave_rts -lBasics_STACK -lrave_private "
+       extraLib = " `xml2-config --libs` -ldl "
+       return apcLib + paths + basicLibs + ruleLib + extraLib
+
+    def isNewer(self, file1, file2):
+        if not os.path.isfile(file1):
+            return 0
+        if not os.path.isfile(file2):
+            return 1
+        if self.modifiedTime(file1) > self.modifiedTime(file2):
+            return 1
+        else:
+            return 0
+    def modifiedTime(self, filename):
+        return os.stat(filename)[stat.ST_MTIME]
 
 class RemoveLogs:
     def __repr__(self):
@@ -61,8 +115,7 @@ class StartStudio:
         print "CARMUSR:", os.environ["CARMUSR"]
         print "CARMTMP:", os.environ["CARMTMP"]
         commandLine = os.path.join(os.environ["CARMSYS"], "bin", "studio")
-        print commandLine
-        print os.popen(commandLine)
+        print os.popen(commandLine).readline()
         sys.exit(0)
     def setUpSuite(self, suite, description):
         pass

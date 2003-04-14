876cbd38f1b4c83ad02d00dd402ad71ce1af60ef
perb <>
2003-04-14 09:58:50 +0000

    Removed code setting up virtual display. That action has been moved to the studio start script since it is best done when the program actually starts (after being sent through LSF to a remote computer).

--- studio.py
+++ studio.py
@@ -4,7 +4,7 @@
 # This plug-in is derived from CarmenConfig which is derived from LSF.
 # The main contribution of this plug-in is being able to allocate a display.
 #
-# $Header: /carm/2_CVS/Testing/TextTest/Attic/studio.py,v 1.3 2003/04/11 08:30:28 perb Exp $
+# $Header: /carm/2_CVS/Testing/TextTest/Attic/studio.py,v 1.4 2003/04/14 11:58:50 perb Exp $
 #
 import carmen, os, plugins
 
@@ -23,58 +23,16 @@ class StudioConfig(carmen.CarmenConfig):
     def getTestCleanup(self):
 	return CleanupTest(self)
 
-def probeDisplay(s):
-    """
-    	Check if screen 0 on display s exists
-	The function returns the display name if it doesn't exist,
-	it throws an exception if the display exists does.
-    """
-    cmd = "xdpyinfo -display " + s + " 2>&1 | grep 'screen #0' > /dev/null"
-    r = os.system(cmd)
-    if r:
-    	return s
-    raise "Display busy"
-
-def findDisplay(host=""):
-    """
-	Look for an available display on current host
-    """
-    for i in range(1,10):
-    	s = host + ":" + str(i)
-    	try:
-	    return probeDisplay(s)
-	except:
-	    pass
-    raise "No available display found on " + host
-
-def createDisplay(disp):
-    """
-    	Start a virtual X-display with a given name
-    """
-    pid = os.fork()
-    os.close(2)
-    if not pid:
-	os.execlp("Xvfb", "Xvfb", disp, "-ac")
-    return pid
-
 class SetupTest(plugins.Action):
     def __repr__(self):
         return "Setup"
     def __init__(self, studio):
 	self.studio = studio
-	self.studio.origDisplay = os.getenv("DISPLAY")
     def __call__(self, test):
         self.describe(test)
-	self.studio.testDisplay = findDisplay(os.getenv("HOST"))
-	self.studio.pid = 0
-	if self.studio.useLSF():
-	    self.studio.pid = createDisplay(self.studio.testDisplay)
-	    os.environ["DISPLAY"] = self.studio.testDisplay
     def setUpSuite(self, suite):
 	self.describe(suite)
 
-
-SIGTERM = 15
 
 class CleanupTest(plugins.Action):
     def __repr__(self):
@@ -83,10 +41,5 @@ class CleanupTest(plugins.Action):
 	self.studio = studio
     def __call__(self, test):
         self.describe(test)
-	if self.studio.pid:
-	    os.kill(self.studio.pid, SIGTERM)
-	    self.studio.pid = 0
-	self.studio.testDisplay = ""
-	os.environ["DISPLAY"] = self.studio.origDisplay
     def setUpSuite(self, suite):
 	self.describe(suite)

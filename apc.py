b362a753a0def635a56137b86e724eda8852a5c3
johani <>
2005-03-30 15:50:05 +0000

    Corrected CleanSubplans to remove the dirs...

--- apc.py
+++ apc.py
@@ -1433,7 +1433,7 @@ class CVSBranchTests(plugins.Action):
 class CleanSubplans(plugins.Action):
     def __init__(self):
         self.config = ApcConfig(None)
-        self.user = "svante" #os.environ["USER"]
+        self.user = os.environ["USER"]
         self.cleanedPlans = 0
         self.totalMem = 0
         self.timeToRemove = time.time()-30*24*3600

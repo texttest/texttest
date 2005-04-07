557a8c5f8fb5b260bfd9a57c08d1e08de0969d94
geoff <>
2005-04-07 13:26:51 +0000

    Porting to windows, removing race condition in GUI

--- knownbugs.py
+++ knownbugs.py
@@ -3,6 +3,7 @@
 import plugins, os, string
 from ConfigParser import ConfigParser
 from predict import FailedPrediction
+from copy import copy
 
 plugins.addCategory("bug", "known bugs", "had known bugs")
 
@@ -12,23 +13,34 @@ class KnownBug(FailedPrediction):
         fullBugText = bugDesc
         briefBugText = "unreported bug"
         if self.isNumber(bugDesc):
-            fullBugText = os.popen("bugcli -b " + bugDesc).read()
+            fullBugText = os.popen(self.getBugcli() + " -b " + bugDesc).read()
             self.bugStatus = self.findStatus(fullBugText)
             briefBugText = "bug " + bugDesc + " (" + self.bugStatus + ")"
         FailedPrediction.__init__(self, self.findCategory(), briefText=briefBugText, freeText=fullBugText, completed = 1)
+    def getBugcli(self):
+        if os.name == "posix":
+            return "bugcli"
+        # Windows requires file extensions, but unknown what it needs for bugcli :)
+        for dir in os.environ["PATH"].split(";"):
+            for file in os.listdir(dir):
+                if file.startswith("bugcli"):
+                    return file
+        return "bugcli"
     def isNumber(self, desc):
         for letter in desc:
             if not letter in string.digits:
                 return 0
         return 1
     def findStatus(self, description):
+        if len(description) == 0:
+            return "no bugcli found"
         for line in description.split("\n"):
             words = line.split()
             if len(words) < 4:
                 continue
             if words[2].startswith("Status"):
                 return words[3]
-        return "UNKNOWN"
+        return "no such bug"
     def findCategory(self):
         if self.bugStatus == "RESOLVED" or self.bugStatus == "CLOSED":
             return "badPredict"
@@ -59,9 +71,9 @@ class CheckForBugs(plugins.Action):
                 for line in open(fileName).xreadlines():
                     for trigger, bugDesc in entryDict.items():
                         if trigger.matches(line):
-                            test.state.setFailedPrediction(KnownBug(bugDesc))
-                            # Tell the GUI
-                            test.notifyChanged()
+                            newState = copy(test.state)
+                            newState.setFailedPrediction(KnownBug(bugDesc))
+                            test.changeState(newState)
         self.unreadBugs(test)
     def readBugs(self, suite):
         if not self.testBugParserMap.has_key(suite):

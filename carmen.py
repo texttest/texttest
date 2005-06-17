3c1eec8d1bc6bbc1e71ccd316d11765e42970910
johana <>
2005-06-17 13:54:54 +0000

    added v12 as major release version

--- carmen.py
+++ carmen.py
@@ -41,7 +41,7 @@ def getConfig(optionMap):
     return CarmenConfig(optionMap)
 
 architectures = [ "i386_linux", "sparc", "sparc_64", "powerpc", "parisc_2_0", "parisc_1_1", "i386_solaris", "ia64_hpux" ]
-majorReleases = [ "8", "9", "10", "11" ]
+majorReleases = [ "8", "9", "10", "11", "12" ]
 
 def getArchitecture(app):
     for version in app.versions:

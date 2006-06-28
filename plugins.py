880df231191ba84f5d2e408a795e5f7e775e567a
geoff <>
2006-06-28 07:26:39 +0000

    No reason not to try a very large number of times, better than leaking directories

--- plugins.py
+++ plugins.py
@@ -310,7 +310,7 @@ def samefile(writeDir, currDir):
         return os.path.normpath(writeDir) == os.path.normpath(currDir)
 
 # Version of rmtree not prone to crashing if directory in use or externally removed
-def rmtree(dir, attempts=10):
+def rmtree(dir, attempts=100):
     if not os.path.isdir(dir):
         print "Write directory", dir, "externally removed"
         return

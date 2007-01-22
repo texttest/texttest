30192ec19795805297da4a34b2767c5de6608294
geoff <>
2007-01-22 15:21:46 +0000

    Introduce generic interceptor to texttest. Not very beautiful but probably less fiddly than trying to do it entirely externally

--- texttest.py
+++ texttest.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+try:
+    # For self-testing, make it easy to intercept methods by providing a natural point to do it
+    # without screwing up the namespace
+    import interceptor
+except ImportError:
+    pass
+
 import sys, os
 sys.path.insert(0, os.path.dirname(sys.argv[0]))
 

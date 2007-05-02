ebd587152066bf9801f060ee009c02cc6f5d1672
geoff <>
2007-05-02 10:43:22 +0000

    CVS feed should just be called master, makes things easier than having .current

--- lib/texttest_version.py
+++ lib/texttest_version.py
@@ -2,7 +2,7 @@
 # This file (at the moment) has the sole purpose of specifying the texttest version number.
 # It's in a separate file to make it easy to find.
 
-version = "3.8.current"
+version = "master"
 
 # Which python version do we require
 

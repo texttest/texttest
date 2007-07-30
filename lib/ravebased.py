677081a71804b9e3ddf57ce6042bd6e33d9addbc
geoff <>
2007-07-30 08:09:50 +0000

    Forward the USER as well

--- lib/ravebased.py
+++ lib/ravebased.py
@@ -339,7 +339,7 @@ class PrepareCarmdataWriteDir(default.PrepareWriteDirectory):
             self.collatePath(test, "$CARMUSR", self.partialCopyTestPath)
 
 def getCrcCompileVars():
-    return [ "CARMSYS", "CARMUSR", "CARMTMP", "CARMGROUP", "_AUTOTEST__LOCAL_COMPILE_" ] 
+    return [ "CARMSYS", "CARMUSR", "CARMTMP", "CARMGROUP", "_AUTOTEST__LOCAL_COMPILE_", "USER" ] 
     
 class RuleBuildActivator(Activator):
     def run(self):

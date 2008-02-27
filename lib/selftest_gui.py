7d014464890c9a44f5dd52b558eb47fbda7879cd
geoff <>
2008-02-27 09:29:12 +0000

    Make selftest_gui actually work, and test it...

--- lib/selftest_gui.py
+++ lib/selftest_gui.py
@@ -1,19 +1,22 @@
 
 # Only used when running the self-tests
 
-import defaultgui
+import default_gui
 
 # Class for importing self tests
-class ImportTestCase(defaultgui.ImportTestCase):
+class ImportTestCase(default_gui.ImportTestCase):
     def addDefinitionFileOption(self):
-        defaultgui.ImportTestCase.addDefinitionFileOption(self)
+        default_gui.ImportTestCase.addDefinitionFileOption(self)
         self.addSwitch("GUI", "Use TextTest GUI", 1)
         self.addSwitch("sGUI", "Use TextTest Static GUI", 0)
     def getOptions(self, suite):
-        options = defaultgui.ImportTestCase.getOptions(self, suite)
+        options = default_gui.ImportTestCase.getOptions(self, suite)
         if self.optionGroup.getSwitchValue("sGUI"):
             options += " -gx"
         elif self.optionGroup.getSwitchValue("GUI"):
             options += " -g"
         return options
 
+class InteractiveActionConfig(default_gui.InteractiveActionConfig):
+    def getReplacements(self):
+        return { default_gui.ImportTestCase : ImportTestCase } 

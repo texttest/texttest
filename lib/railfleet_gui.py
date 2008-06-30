3ff30faae5bd43677702c7de5e733b43cd9db98d
henning <>
2008-06-30 13:23:00 +0000

    configuration for the railfleet gui. Currently to support ruleset builds when recording new tests.

---
+++ lib/railfleet_gui.py
@@ -0,0 +1,19 @@
+import ravebased_gui, default_gui, cvs, ravebased, os, shutil, subprocess
+
+# Allow manual specification of a ruleset, and two auto-replays needed for macro recorder...
+class RecordTest(default_gui.RecordTest):
+    def __init__(self, *args):
+        default_gui.RecordTest.__init__(self, *args)
+        self.optionGroup.addOption("rulecomp", "Compile this ruleset first")
+        self.changedUseCaseVersion = ""
+    def updateOptions(self):
+        retValue = default_gui.RecordTest.updateOptions(self)
+        self.optionGroup.setOptionValue("rulecomp", "")
+        self.optionGroup.setPossibleValues("rulecomp", [])
+        return retValue
+    def getCommandLineKeys(self, *args):
+        return default_gui.RecordTest.getCommandLineKeys(self, *args) + [ "rulecomp" ]
+
+class InteractiveActionConfig(cvs.InteractiveActionConfig):
+    def getReplacements(self):
+        return { default_gui.RecordTest      : RecordTest } 

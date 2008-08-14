978b61b9388cad84cb26d9ae07d8d38739196f8e
geoff <>
2008-08-14 12:55:47 +0000

    Need to enforce presence of Python 2.5 on Windows now

--- lib/texttest_version.py
+++ lib/texttest_version.py
@@ -2,6 +2,7 @@
 # This file (at the moment) has the sole purpose of specifying the texttest version number.
 # It's in a separate file to make it easy to find.
 
+import os
 version = "master"
 
 # Note: Decided it's not a good idea to require debug versions here. Even if we have Python 2.4.3 it's
@@ -9,8 +10,12 @@ version = "master"
 
 # Which python version do we require?
 
-required_python_version = (2, 4, 0)
-
+if os.name == "posix":
+    required_python_version = (2, 4, 0)
+else:
+    # We depend on Python 2.5's ctypes module now to kill processes on Windows.
+    # Python 2.5.0 known to have serious issues around subprocess handling.
+    required_python_version = (2, 5, 1)
+    
 # Which pygtk version do we require?
-
 required_pygtk_version = (2, 10, 0)

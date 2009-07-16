da7357630bead68d316bbc7f44fbdc34cb220dd8
Geoff Bache <geoff.bache@jeppesen.com>
2009-07-16 18:46:02 +0200

    Use sys.argv[0], which is more reliable

--- lib/default/rundependent.py
+++ lib/default/rundependent.py
@@ -5,7 +5,7 @@ import os
 
 if __name__ == "__main__":
     import sys
-    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
 
 import plugins, logging
 from ndict import seqdict

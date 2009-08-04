04ab5891450b3cb2a531d8a02893a63b0de29887
Geoff Bache <geoff.bache@jeppesen.com>
2009-08-04 10:26:34 +0200

    Don't crash if GTK can't be found

--- lib/default/gtkgui/__init__.py
+++ lib/default/gtkgui/__init__.py
@@ -2,10 +2,10 @@
 # One basic action that wants to bypass the GTK imports
 
 import plugins
-from guiutils import GUIConfig
         
 class DocumentGUIConfig(plugins.Action):
     def setUpApplication(self, app):
+        from guiutils import GUIConfig
         guiConfig = GUIConfig(False, [ app ], GUIConfig.getDefaultColours(), GUIConfig.getDefaultAccelerators())
         for key in sorted(guiConfig.configDir.keys()):
             docOutput = guiConfig.configDocs[key]

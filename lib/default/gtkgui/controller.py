a8b1a1f7dcc222408f15155c9986711e3e032bd8
Geoff Bache <geoff.bache@jeppesen.com>
2009-10-22 18:13:16 +0200

    Very useful diagnostic added

--- lib/default/gtkgui/controller.py
+++ lib/default/gtkgui/controller.py
@@ -687,5 +687,6 @@ class InteractiveActionHandler:
     def classValid(self, className, app):
         for config in self.getAllIntvConfigs([ app ]):
             if not config.isValid(className):
+                self.diag.info(str(config.__class__) + " configuration rejected " + str(className) + " class as invalid")
                 return False
         return True

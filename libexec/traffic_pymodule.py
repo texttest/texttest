de20bbf38e998db19efa724740890cc3dfe48d7a
Geoff Bache <geoff.bache@jeppesen.com>
2010-02-09 09:40:44 +0100

    Keep track of the modules locally also

--- libexec/traffic_pymodule.py
+++ libexec/traffic_pymodule.py
@@ -1,65 +1,69 @@
 
 import sys
 
-class ModuleOrObjectProxy:
-    AttributeProxy = None
-    def __init__(self, modOrObjName):
-        self.modOrObjName = modOrObjName
+class ModuleProxy:
+    def __init__(self, name):
+        self.name = name
+
+    def __getattr__(self, attrname):
+        return self.AttributeProxy(self, self, attrname)
+
+    class InstanceProxy:
+        def __init__(self, instanceName, moduleProxy):
+            self.name = instanceName
+            self.moduleProxy = moduleProxy
+
+        def __getattr__(self, attrname):
+            return self.moduleProxy.AttributeProxy(self, self.moduleProxy, attrname)
+
+    class ExceptionProxy(InstanceProxy, Exception):
+        def __str__(self):
+            return self.__getattr__("__str__")()
+
         
-    def __getattr__(self, name):
-        return self.AttributeProxy(self, name)
-        
+    class AttributeProxy:
+        def __init__(self, modOrObjProxy, moduleProxy, attributeName):
+            self.modOrObjProxy = modOrObjProxy
+            self.moduleProxy = moduleProxy
+            self.attributeName = attributeName
 
-class AttributeProxy:
-    ModuleOrObjectProxy = None
-    ExceptionProxy = None
-    def __init__(self, modOrObjProxy, attributeName):
-        self.modOrObjProxy = modOrObjProxy
-        self.attributeName = attributeName
-        
-    def __getattr__(self, name):
-        return self.__class__(self.modOrObjProxy, self.attributeName + "." + name)
+        def __getattr__(self, name):
+            return self.__class__(self.modOrObjProxy, self.moduleProxy, self.attributeName + "." + name)
 
-    def __call__(self, *args, **kw):
-        sock = self.createAndSend(*args, **kw)
-        sock.shutdown(1)
-        response = sock.makefile().read()
-        return self.handleResponse(response, self.ModuleOrObjectProxy)
+        def __call__(self, *args, **kw):
+            sock = self.createAndSend(*args, **kw)
+            sock.shutdown(1)
+            response = sock.makefile().read()
+            return self.handleResponse(response, self.moduleProxy.InstanceProxy)
 
-    def handleResponse(self, response, cls):
-        if response.startswith("raise "):
-            rest = response.replace("raise ", "")
-            raise self.handleResponse(rest, self.ExceptionProxy)
-        else:
-            def Instance(className, instanceName):
-                setattr(self.modOrObjProxy, className, cls)
-                return cls(instanceName)
-            return eval(response)        
+        def handleResponse(self, response, cls):
+            if response.startswith("raise "):
+                rest = response.replace("raise ", "")
+                raise self.handleResponse(rest, self.moduleProxy.ExceptionProxy)
+            else:
+                def Instance(className, instanceName):
+                    setattr(self.moduleProxy, className, cls)
+                    return cls(instanceName, self.moduleProxy)
+                return eval(response)        
 
-    def createAndSend(self, *args, **kw):
-        sock = self.createSocket()
-        text = "SUT_PYTHON_CALL:" + self.modOrObjProxy.modOrObjName + ":SUT_SEP:" + self.attributeName + ":SUT_SEP:" + repr(args) + ":SUT_SEP:" + repr(kw)
-        sock.sendall(text)
-        return sock
-    
-    def createSocket(self):
-        import os, socket
-        servAddr = os.getenv("TEXTTEST_MIM_SERVER")
-        if servAddr:
-            host, port = servAddr.split(":")
-            serverAddress = (host, int(port))
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(serverAddress)
+        def createAndSend(self, *args, **kw):
+            sock = self.createSocket()
+            text = "SUT_PYTHON_CALL:" + self.modOrObjProxy.name
+            text += ":SUT_SEP:" + self.attributeName
+            text += ":SUT_SEP:" + repr(args) + ":SUT_SEP:" + repr(kw)
+            sock.sendall(text)
             return sock
 
-class ExceptionProxy(ModuleOrObjectProxy, Exception):
-    AttributeProxy = None
-    def __str__(self):
-        return self.AttributeProxy(self, "__str__")()
+        def createSocket(self):
+            import os, socket
+            servAddr = os.getenv("TEXTTEST_MIM_SERVER")
+            if servAddr:
+                host, port = servAddr.split(":")
+                serverAddress = (host, int(port))
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect(serverAddress)
+                return sock
 
-ModuleOrObjectProxy.AttributeProxy = AttributeProxy
-AttributeProxy.ModuleOrObjectProxy = ModuleOrObjectProxy
-AttributeProxy.ExceptionProxy = ExceptionProxy
-ExceptionProxy.AttributeProxy = AttributeProxy
 
-sys.modules[__name__] = ModuleOrObjectProxy(__name__)
+
+sys.modules[__name__] = ModuleProxy(__name__)

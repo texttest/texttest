519296995d3b3fcb54a97e51f0144ea8f5d0e971
Geoff Bache <geoff.bache@jeppesen.com>
2010-09-15 16:11:27 +0200

    Another interceptor hook no longer needed

--- libexec/startXvfb.py
+++ libexec/startXvfb.py
@@ -8,11 +8,6 @@
 #   way to tell these signals from ordinary job control
 # - Clean up after Xvfb as it leaks lock files from time to time.
 
-try:
-    import startxvfb_interceptor # the usual test hook...
-except ImportError: # pragma: no cover - all tests need an interceptor to be able to control PID
-    pass
-
 import os, signal, sys, subprocess
 from socket import gethostname
 

69ba233cda38f2b91f596cac472cd2d185cc0ec6
Geoff Bache <geoff.bache@jeppesen.com>
2010-11-02 09:35:19 +0100

    Cutting down colourer code so it isn't more complex than we need

--- lib/default/colorer.py
+++ lib/default/colorer.py
@@ -5,18 +5,11 @@ import logging
 NORMAL = 0
 RED = 1
 GREEN = 2
-YELLOW = 3
 
 initialEmitter = None
 
 # now we patch Python code to add color support to logging.StreamHandler
 def add_coloring_to_emit_windows(fn, desired):
-        # add methods we need to the class
-    def _out_handle(self):
-        import ctypes
-        return ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
-    out_handle = property(_out_handle)
-
     def _set_color(self, code):
         import ctypes
         # Constants from the Windows API
@@ -27,45 +20,17 @@ def add_coloring_to_emit_windows(fn, desired):
     setattr(logging.StreamHandler, '_set_color', _set_color)
 
     def new(*args):
+        # wincon.h
         FOREGROUND_BLUE      = 0x0001 # text color contains blue.
         FOREGROUND_GREEN     = 0x0002 # text color contains green.
         FOREGROUND_RED       = 0x0004 # text color contains red.
         FOREGROUND_INTENSITY = 0x0008 # text color is intensified.
         FOREGROUND_WHITE     = FOREGROUND_BLUE|FOREGROUND_GREEN |FOREGROUND_RED
-       # winbase.h
-        STD_INPUT_HANDLE = -10
-        STD_OUTPUT_HANDLE = -11
-        STD_ERROR_HANDLE = -12
-
-        # wincon.h
-        FOREGROUND_BLACK     = 0x0000
-        FOREGROUND_BLUE      = 0x0001
-        FOREGROUND_GREEN     = 0x0002
-        FOREGROUND_CYAN      = 0x0003
-        FOREGROUND_RED       = 0x0004
-        FOREGROUND_MAGENTA   = 0x0005
-        FOREGROUND_YELLOW    = 0x0006
-        FOREGROUND_GREY      = 0x0007
-        FOREGROUND_INTENSITY = 0x0008 # foreground color is intensified.
-
-        BACKGROUND_BLACK     = 0x0000
-        BACKGROUND_BLUE      = 0x0010
-        BACKGROUND_GREEN     = 0x0020
-        BACKGROUND_CYAN      = 0x0030
-        BACKGROUND_RED       = 0x0040
-        BACKGROUND_MAGENTA   = 0x0050
-        BACKGROUND_YELLOW    = 0x0060
-        BACKGROUND_GREY      = 0x0070
-        BACKGROUND_INTENSITY = 0x0080 # background color is intensified.     
  
         if desired == RED:
             color = FOREGROUND_RED | FOREGROUND_INTENSITY
-        elif desired == YELLOW:
-            color = FOREGROUND_YELLOW | FOREGROUND_INTENSITY
         elif desired == GREEN:
             color = FOREGROUND_GREEN | FOREGROUND_INTENSITY
-        else:
-            color =  FOREGROUND_WHITE
         
         args[0]._set_color(color) #apply color
         ret = fn(*args) #allow the logger to do it's thing
@@ -79,12 +44,8 @@ def add_coloring_to_emit_ansi(fn, desired):
     def new(*args):
         if desired == RED:
             color = '\x1b[31m' # red
-        elif desired == YELLOW:
-            color = '\x1b[33m' # yellow
         elif desired == GREEN:
             color = '\x1b[32m' # green 
-        else:
-            color = '\x1b[0m' # normal
         #wrap the message in the color tags
         args[1].msg = color + args[1].msg +  '\x1b[0m' #always end in normal
         

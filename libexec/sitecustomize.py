f2cffbc0c47a5009be5513b112e7dfcd2aa0b8c6
Geoff Bache <geoff.bache@jeppesen.com>
2010-11-16 11:01:26 +0100

    Load the original sitecustomize first, to take advantage of its PYTHONPATH manipulations

--- libexec/sitecustomize.py
+++ libexec/sitecustomize.py
@@ -9,13 +9,14 @@ def trySetupCoverage(): # pragma: no cover - can hardly measure coverage here :)
         pass
 
 
-def doInterceptions():
+def loadTestCustomize():
     try:
         # Generic file name to customize the behaviour of Python per test
         import testcustomize
     except ImportError:
         pass
-    
+
+def trySetupCaptureMock():
     pythonVarStr = os.getenv("TEXTTEST_MIM_PYTHON")
     if pythonVarStr and sys.version_info[:2] >= (2, 4):
         # Capturemock uses Python 2.4 syntax, won't work on earlier versions
@@ -26,18 +27,23 @@ def doInterceptions():
         if ignoreVar:
             ignoreCallers = ignoreVar.split(",")
         interceptPython(attributeNames, ignoreCallers)
-        del os.environ["TEXTTEST_MIM_PYTHON"] # Guard against double setup when in the self-tests
 
-    # Need to load the "real" sitecustomize now
+def loadRealSiteCustomize(): # pragma: no cover - coverage not set up yet
+    # must do this before setting up coverage as real sitecustomize might
+    # manipulate PYTHONPATH in such a way that coverage can be found
     import imp
+    os.environ["TEXTTEST_SITECUSTOMIZE"] = "1" # Guard against double setup when in the self-tests
     myDir = os.path.dirname(__file__)
     pos = sys.path.index(myDir)
     try:
         modInfo = imp.find_module("sitecustomize", sys.path[pos + 1:])
         imp.load_module("sitecustomize", *modInfo)
     finally:
-        if pythonVarStr:
-            os.environ["TEXTTEST_MIM_PYTHON"] = pythonVarStr
+        if os.environ.has_key("TEXTTEST_SITECUSTOMIZE"):
+            del os.environ["TEXTTEST_SITECUSTOMIZE"]
 
-trySetupCoverage() # pragma: no cover - coverage not set up yet
-doInterceptions() # pragma: no cover - coverage not set up yet
+loadRealSiteCustomize() # pragma: no cover - coverage not set up yet
+if not os.environ.has_key("TEXTTEST_SITECUSTOMIZE"): # pragma: no cover - coverage not set up yet
+    trySetupCoverage()
+    loadTestCustomize()
+    trySetupCaptureMock() 

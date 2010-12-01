ad4d92d67f9fdd2bce76d5a4b3c0678a28bb9d9d
Geoff Bache <geoff.bache@jeppesen.com>
2010-12-01 12:04:13 +0100

    Don't generate index pages if no apps under them have been generated

--- lib/default/batch/batchutils.py
+++ lib/default/batch/batchutils.py
@@ -1,5 +1,23 @@
-import plugins
-import datetime
+import plugins, datetime
+
+class BatchVersionFilter:
+    def __init__(self, batchSession):
+        self.batchSession = batchSession
+
+    def verifyVersions(self, app):
+        badVersion = self.findUnacceptableVersion(app)
+        if badVersion is not None:
+            raise plugins.TextTestError, "unregistered version '" + badVersion + "' for " + self.batchSession + " session."
+
+    def findUnacceptableVersion(self, app):
+        if app.getCompositeConfigValue("batch_use_version_filtering", self.batchSession) != "true":
+            return
+        
+        allowedVersions = app.getCompositeConfigValue("batch_version", self.batchSession)
+        for version in app.versions:
+            if len(version) > 0 and not version in allowedVersions:
+                return version
+
 
 def calculateBatchDate():
     # Batch mode uses a standardised date that give a consistent answer for night-jobs.

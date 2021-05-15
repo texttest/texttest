da2b3e953f0e757501d620011187f70d70526489
Geoff Bache <geoff.bache@pobox.com>
2021-05-15 19:21:12 +0200

    Bump required version to 3.6, as 3.4 msys version no longer used on Windows

--- texttestlib/texttest_version.py
+++ texttestlib/texttest_version.py
@@ -8,7 +8,7 @@ version = "4.0.7"
 # pretty likely 2.4.2 works OK unless we know otherwise. The interface shouldn't be different at least.
 
 # Which python version do we require?
-required_python_version = (3, 4, 0)
+required_python_version = (3, 6, 0)
 
 # Which pygtk version do we require?
 required_pygtk_version = (3, 18, 0)
